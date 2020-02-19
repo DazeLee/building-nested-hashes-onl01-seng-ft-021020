704459e271fe23e5a1a57d34d53ab8ce40e78c2c
Daisy Olmo-Rivera <dazelee@yahoo.com>
2020-02-19 16:17:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
- :patriaich :matriarch :hero :hero_friends     #your key/value pairs here!
+ :patriaich => {},
+ :matriarch => {},
+ :hero => {},
+ :hero_friends => []    #your key/value pairs here!
       }, 
+    
     :capulet => {
-:patriaich :matriarch :heroine :heroine_friends      #your key/value pairs here!
+:patriaich => {},
+:matriarch => {},
+:heroine => {},
+:heroine_friends  => []    #your key/value pairs here!
       }}
 
   
